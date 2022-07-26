acc7a4afeefd3467fcde0c37787ccaee935d1a65
Belial <3393215312@qq.com>
2022-07-26 22:15:29 +0800

    hot-fix Commit

--- src/main/java/com/own/git/Test1.java
+++ src/main/java/com/own/git/Test1.java
@@ -7,5 +7,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("GIT");
         System.out.println("GIT2");
+        System.out.println("hot-fix test");
     }
 }
